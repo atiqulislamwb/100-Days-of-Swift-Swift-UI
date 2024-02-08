14042123e789603b94c06a308f2d5290f28fab72
atiqulislamwb <atiqul@lintechgroup.co.uk>
2024-02-08 12:44:35 +0600

    progress on learning

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Atiqul Islam on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
