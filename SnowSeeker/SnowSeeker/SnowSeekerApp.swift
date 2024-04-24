b9bd1726575d0b0680e9ea1432a4f5d08111ab6b
atiqulislamwb <atiqul@lintechgroup.co.uk>
2024-04-24 13:01:32 +0600

    last project of 100 days swift ui-snowseeker

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Atiqul Islam on 24/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
